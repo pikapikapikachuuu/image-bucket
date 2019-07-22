8de143fa17da415fb5396d96265c8499bb64bcb7
pikachu <hooraypikachu@gmail.com>
2019-07-22 15:36:09 +0800

    tests for objects

--- test/lifter.test.js
+++ test/lifter.test.js
@@ -29,6 +29,7 @@ describe('S3 Bucket Lifter', () => {
   describe('when constructing', () => {
     it('should configure AWS access', () => {
       const lifter = new Lifter()
+
       td.verify(AWS.config.loadFromPath(td.matchers.anything()), { times: 1 })
     })
   })
@@ -36,30 +37,123 @@ describe('S3 Bucket Lifter', () => {
   describe('when creating a bucket', () => {
     beforeEach(() => {
       td.when(AWS.S3.prototype.createBucket({ Bucket: 'test-bucket' })).thenReturn({
-        promise: td.when(td.function()()).thenResolve({ status: 200 })
+        promise: td.when(td.function()()).thenResolve({ Status: 200 })
       })
     })
 
     it('should call AWS.S3.createBucket and resolve', async () => {
       const lifter = new Lifter()
       const result = await lifter.createBucket('test-bucket')
-      await td.verify(AWS.S3.prototype.createBucket(td.matchers.anything()), { times: 1 })
-      result.status.should.be.equal(200)
+
+      td.verify(AWS.S3.prototype.createBucket(td.matchers.anything()), { times: 1 })
+
+      result.Status.should.be.equal(200)
     })
   })
 
   describe('when deleting a bucket', () => {
     beforeEach(() => {
       td.when(AWS.S3.prototype.deleteBucket({ Bucket: 'test-bucket' })).thenReturn({
-        promise: td.when(td.function()()).thenResolve({ status: 200 })
+        promise: td.when(td.function()()).thenResolve({ Status: 200 })
       })
     })
 
     it('should call AWS.S3.deleteBucket and resolve', async () => {
       const lifter = new Lifter()
       const result = await lifter.deleteBucket('test-bucket')
-      await td.verify(AWS.S3.prototype.deleteBucket(td.matchers.anything()), { times: 1 })
-      result.status.should.be.equal(200)
+
+      td.verify(AWS.S3.prototype.deleteBucket(td.matchers.anything()), { times: 1 })
+
+      result.Status.should.be.equal(200)
+    })
+  })
+
+  describe('when putting an object', () => {
+    beforeEach(() => {
+      td.when(AWS.S3.prototype.putObject(td.matchers.argThat((args) => Object.keys(args).length === 3))).thenReturn({
+        promise: td.when(td.function()()).thenResolve({ Status: 200 })
+      })
+    })
+
+    it('should call AWS.S3.putObject and resolve', async () => {
+      const lifter = new Lifter()
+      const result = await lifter.putObject('test-bucket', 'test-key', 'test-data')
+
+      const captor = td.matchers.captor()
+      td.verify(AWS.S3.prototype.putObject(captor.capture()), { times: 1 })
+      captor.value.Bucket.should.be.equal('test-bucket')
+      captor.value.Key.should.be.equal('test-key')
+      captor.value.Body.should.be.equal('test-data')
+
+      result.Status.should.be.equal(200)
+    })
+  })
+
+  describe('when getting an object', () => {
+    beforeEach(() => {
+      td.when(AWS.S3.prototype.getObject(td.matchers.argThat((args) => Object.keys(args).length === 2))).thenReturn({
+        promise: td.when(td.function()()).thenResolve({ Status: 200, Body: 'test-data' })
+      })
+    })
+
+    it('should call AWS.S3.getObject and resolve', async () => {
+      const lifter = new Lifter()
+      const result = await lifter.getObject('test-bucket', 'test-key')
+
+      const captor = td.matchers.captor()
+      td.verify(AWS.S3.prototype.getObject(captor.capture()), { times: 1 })
+      captor.value.Bucket.should.be.equal('test-bucket')
+      captor.value.Key.should.be.equal('test-key')
+
+      result.Status.should.be.equal(200)
+      result.Body.should.be.equal('test-data')
+    })
+  })
+
+  describe('when deleting an object', () => {
+    beforeEach(() => {
+      td.when(AWS.S3.prototype.deleteObject(td.matchers.argThat((args) => Object.keys(args).length === 2))).thenReturn({
+        promise: td.when(td.function()()).thenResolve({ Status: 200 })
+      })
+    })
+
+    it('should call AWS.S3.deleteObject and resolve', async () => {
+      const lifter = new Lifter()
+      const result = await lifter.deleteObject('test-bucket', 'test-key')
+
+      const captor = td.matchers.captor()
+      td.verify(AWS.S3.prototype.deleteObject(captor.capture()), { times: 1 })
+      captor.value.Bucket.should.be.equal('test-bucket')
+      captor.value.Key.should.be.equal('test-key')
+
+      result.Status.should.be.equal(200)
+    })
+  })
+
+  describe('when listing objects', () => {
+    beforeEach(() => {
+      td.when(AWS.S3.prototype.listObjects(td.matchers.argThat((args) => Object.keys(args).length >= 2))).thenReturn({
+        promise: td.when(td.function()()).thenResolve({
+          Status: 200,
+          IsTruncated: false,
+          Contents: [
+            { Key: '1.jpg' },
+            { Key: '2.jpg' }
+          ]
+        })
+      })
+    })
+
+    it('should call AWS.S3.listObjects and resolve', async () => {
+      const lifter = new Lifter()
+      const result = await lifter.listObjects('test-bucket')
+
+      const captor = td.matchers.captor()
+      td.verify(AWS.S3.prototype.listObjects(captor.capture()))
+      captor.value.Bucket.should.be.equal('test-bucket')
+      captor.value.Key.should.be.equal('test-key')
+
+      result.Status.should.be.equal(200)
     })
   })
 })
